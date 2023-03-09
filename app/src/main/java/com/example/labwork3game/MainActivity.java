400f8ad69b1b689de19b4fa16e19628ea0a1e9b1
macross0 <maxnekuga@gmail.com>
2023-03-09 10:43:01 +0200

    Added switching to GameActivity

--- app/src/main/java/com/example/labwork3game/MainActivity.java
+++ app/src/main/java/com/example/labwork3game/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.labwork3game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View GameActivitySwitcher = findViewById(R.id.start_button);
+        GameActivitySwitcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToGameActivity();
+            }
+        });
+    }
+
+    private void switchToGameActivity() {
+        Intent switchToGameActivityIntent = new Intent(this, GameActivity.class);
+        startActivity(switchToGameActivityIntent);
     }
 }
